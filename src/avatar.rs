bb0363b4b97a1ec893c9e738d52c4b687c0bf1a6
Martin Kavík <martin.kavik@gmail.com>
2019-06-29 22:19:51 +0200

    fix avatar in page header

--- src/avatar.rs
+++ src/avatar.rs
@@ -3,18 +3,18 @@ use crate::asset;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Avatar(Cow<'static, str>);
+pub struct Avatar(Option<Cow<'static, str>>);
 
 impl Avatar {
     pub fn new(url: Option<impl Into<Cow<'static, str>>>) -> Self
     {
-        match url {
-            Some(url) => Avatar(url.into()),
-            None => Avatar(asset::default_avatar().into_url().into())
-        }
+        Avatar(url.map(Into::into))
     }
 
-    pub fn src(&self) -> &str {
-        &self.0
+    pub fn src(&self) -> String {
+        match &self.0 {
+            Some(url) if !url.is_empty() => url.to_string(),
+            _ => asset::default_avatar().into_url()
+        }
     }
 }
